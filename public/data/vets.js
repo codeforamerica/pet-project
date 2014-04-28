17031aa8c91587319db6b570f71a76a850879ced
Maksim Pecherskiy <max@maksimize.com>
2014-04-28 16:03:04 -0700

    Adding vets geojson.

---
+++ public/data/vets.js
@@ -0,0 +1,425 @@
+var vets = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "VCA East Mill Plain Animal Hospital",
+        "address": "416 NE 112th Ave",
+        "city": "Vancouver",
+        "zip": 98684,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Vancouver",
+        "GeocodeAdminAreaLevel1": "Washington",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "416 NE 112th Ave, Vancouver, 98684"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.5582618,
+          45.6241673
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Lombard Animal Hospital",
+        "address": "607 NE Lombard St",
+        "city": "Portland",
+        "zip": 97211,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "607 NE Lombard St, Portland, 97211"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.6590945,
+          45.5774188
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Mt Tabor Veterinary Care",
+        "address": "4246 SE Belmont St",
+        "city": "Portland",
+        "zip": 97215,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "4246 SE Belmont St, Portland, 97215"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.6184817,
+          45.5164497
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Rose City Veterinary Hospital",
+        "address": "809 SE Powell Blvd",
+        "city": "Portland",
+        "zip": 97202,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "809 SE Powell Blvd, Portland, 97202"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.6574519,
+          45.5017386
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Vista Pet Hospital",
+        "address": "3200 SW 87th Ave",
+        "city": "Portland",
+        "zip": 97225,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "3200 SW 87th Ave, Portland, 97225"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.766484,
+          45.497083
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Irvington Veterinary Clinic",
+        "address": "1427 NE Fremont St",
+        "city": "Portland",
+        "zip": 97212,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "1427 NE Fremont St, Portland, 97212"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.6513483,
+          45.5486006
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pearl Animal Hospital",
+        "address": "1250 NW 10th Ave",
+        "city": "Portland",
+        "zip": 97209,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "1250 NW 10th Ave, Portland, 97209"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.6813323,
+          45.53195909999999
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Hawthorne Veterinary Clinic",
+        "address": "1431 SE 23rd Ave",
+        "city": "Portland",
+        "zip": 97214,
+        "GeocodeStatus": "APPROXIMATE",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "1431 SE 23rd Ave, Portland, 97214"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.642823,
+          45.512358
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Bethany Family Pet Clinic",
+        "address": "15166 NW Central Dr",
+        "city": "Portland",
+        "zip": 97229,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "15166 NW Central Dr, Portland, 97229"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.8334438,
+          45.5537766
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Mt Scott Animal Clinic",
+        "address": "8401 SE Ellis St",
+        "city": "Portland",
+        "zip": 97266,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "8401 SE Ellis St, Portland, 97266"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.5766359,
+          45.48237779999999
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Peninsula Dog & Cat Clinic",
+        "address": "5313 N Interstate Ave",
+        "city": "Portland",
+        "zip": 97217,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "5313 N Interstate Ave, Portland, 97217"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.6826601,
+          45.5613769
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Northwest Neighborhood Veterinary Hospital",
+        "address": "2680 NW Thurman St",
+        "city": "Portland",
+        "zip": 97210,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "2680 NW Thurman St, Portland, 97210"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.7065173,
+          45.5351036
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Best Friends Veterinary Medical Center",
+        "address": "0123 SW Hamilton St",
+        "city": "Portland",
+        "zip": 97239,
+        "GeocodeStatus": "RANGE_INTERPOLATED",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "0123 SW Hamilton St, Portland, 97239"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.6764842,
+          45.4908894
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "VCA North Portland Veterinary Hospital",
+        "address": "3000 N Lombard St",
+        "city": "Portland",
+        "zip": 97217,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "3000 N Lombard St, Portland, 97217"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.6979035,
+          45.5768468
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Banfield Pet Hospital",
+        "address": "7535 SW Barnes Road",
+        "city": "Portland",
+        "zip": 97225,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "7535 SW Barnes Road, Portland, 97225"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.7550257,
+          45.5155413
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Powell Boulevard Veterinary Clinic",
+        "address": "3654 SE Powell Blvd",
+        "city": "Portland",
+        "zip": 97202,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "3654 SE Powell Blvd, Portland, 97202"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.6250588,
+          45.49691259999999
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "DoveLewis Emergency Animal Hospital",
+        "address": "1945 NW Pettygrove St",
+        "city": "Portland",
+        "zip": 97209,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "1945 NW Pettygrove St, Portland, 97209"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.6913345,
+          45.5330605
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pet Samaritan Clinic",
+        "address": "2519 E Burnside St",
+        "city": "Portland",
+        "zip": 97214,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "2519 E Burnside St, Portland, 97214"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.6400462,
+          45.5230995
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Alberta Veterinary Care",
+        "address": "1737 NE Alberta",
+        "city": "Portland",
+        "zip": 97211,
+        "GeocodeStatus": "RANGE_INTERPOLATED",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "1737 NE Alberta, Portland, 97211"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.6469642,
+          45.5590936
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Montavilla Animal Clinic",
+        "address": "7714 SE Stark St",
+        "city": "Portland",
+        "zip": 97215,
+        "GeocodeStatus": "ROOFTOP",
+        "GeocodeLocality": "Portland",
+        "GeocodeAdminAreaLevel1": "Oregon",
+        "GeocodeCountry": "United States",
+        "GeocodeAddress": "7714 SE Stark St, Portland, 97215"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.5839294,
+          45.518871
+        ]
+      }
+    }
+  ]
+}
